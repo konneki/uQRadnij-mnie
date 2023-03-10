db05db9b7fdeafe63d9eaf373d4a8ee95b9340c9
Konneki <konnekdaniel@gmail.com>
2023-03-10 13:13:20 +0100

    Initial Commit

---
+++ uQRadnij mnie/uQRadnij_mnieApp.swift
@@ -0,0 +1,17 @@
+//
+//  uQRadnij_mnieApp.swift
+//  uQRadnij mnie
+//
+//  Created by Daniel Konnek on 10.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct uQRadnij_mnieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
